843a50122c8e890e1c34fcfdf4f8ca394eee9fed
Maycon Pires <mayconpires@gmail.com>
2017-07-06 14:52:54 -0300

    Projeto .Net

---
+++ DataStructure/DataStructure.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataStructure.UI.MvcApplication" Language="C#" %>
---
+++ DataStructure/DataStructure.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataStructure.UI.Startup))]
+namespace DataStructure.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
